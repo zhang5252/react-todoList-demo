f225fe206b2e9efb73e89b1be105f386d9c6e9ed
zhangjingyi <z15222565322@126.com>
2019-10-10 10:16:27 +0800

    [IMP] 背景图片切换功能

--- src/App.js
+++ src/App.js
@@ -4,34 +4,63 @@ import TodoApp from './TodoApp';
 import Header from './Header';
 import Footer from './Footer';
 
-
 class App extends Component {
+  onRef = (ref) => {
+    this.background = ref
+  }
   render() {
     return (
       <div className='App'>
-        <Background />
-        <Header />
-        <TodoApp/>
+        <Background onRef={this.onRef} />
+        <Header
+          onClick={
+            () => {
+              this.background.onChange && this.background.onChange()
+            }
+          }
+        />
+        <TodoApp />
         <Footer />
       </div>
     );
   }
 }
 
-const Background = () => {
-  const imageNmu = 3;
-  const image = require("./images/" + imageNmu + ".jpg")
-  const bgStyle = {
-    width: '100%',
-    high: '400px',
-    backgroundImage: `url(${image})`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-  };
-  return (
-    <div className='cover' style={bgStyle}>
-    </div>
-  )
+class Background extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageNum: 1,
+    }
+  }
+  onChange = () => {
+    let index = this.state.imageNum + 1
+    if (index > 3) {
+      index = 1
+    }
+    this.setState({
+      imageNum: index
+    })
+  }
+
+  componentDidMount() {
+    this.props.onRef && this.props.onRef(this)
+  }
+
+  render() {
+    const image = require("./images/" + this.state.imageNum + ".jpg")
+    const bgStyle = {
+      width: '100%',
+      high: '400px',
+      backgroundImage: `url(${image})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+    };
+    return (
+      <div className='cover' style={bgStyle}>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
   render() {
     return (
       <div className="App-header">
         <h1>TodoList</h1>
-        {/* <button className='button-inline'>Change</button> */}
+        <button onClick={this.props.onClick}>Change</button>
       </div>
     )
   }
