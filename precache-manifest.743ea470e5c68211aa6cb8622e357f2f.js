2e3b567db4362b8015da8bcd5b12a5c837fa3c6a
zhangjingyi <z15222565322@126.com>
2019-10-14 16:47:06 +0800

    Updates

---
+++ precache-manifest.743ea470e5c68211aa6cb8622e357f2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8428660369b9ac943e381241dcf49088",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e845e776bc29ac1081cf",
+    "url": "/static/css/main.8aea1b86.chunk.css"
+  },
+  {
+    "revision": "6b86139b66b8aadfe433",
+    "url": "/static/js/2.4f84e291.chunk.js"
+  },
+  {
+    "revision": "e845e776bc29ac1081cf",
+    "url": "/static/js/main.859099f5.chunk.js"
+  },
+  {
+    "revision": "cf9392a8dd544c831453",
+    "url": "/static/js/runtime~main.d7521a1f.js"
+  },
+  {
+    "revision": "cd02fe7d90997fe8c70532e62fbe81f8",
+    "url": "/static/media/1.cd02fe7d.jpg"
+  },
+  {
+    "revision": "fe0859e010d843b9d54c00abd22e8ccd",
+    "url": "/static/media/2.fe0859e0.jpg"
+  },
+  {
+    "revision": "68ca4922ca41ede4811460a789799bb7",
+    "url": "/static/media/3.68ca4922.jpg"
+  }
+]);
